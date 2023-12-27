d14e429ead877fed07d12a02204ae81c8778fa9b
BelalEbrahim <110784708+BelalEbrahim@users.noreply.github.com>
2023-12-27 06:54:42 +0200

    Update SimpleDA.asm

--- SimpleDA.asm
+++ SimpleDA.asm
@@ -6,7 +6,7 @@ start:
     mov ax, 13h     ; Set video mode (320x200 pixels, 256 colors)
     int 10h
 
-draw_snake:
+draw:
     mov ah, 0ch     ; Set pixel color
     mov al, 02h     ; Color (green)
     mov bh, 0       ; Page number
@@ -14,7 +14,7 @@ draw_snake:
     mov dx, y_pos   ; Y position
     int 10h
 
-    ; Move the snake
+    ; Move the point
     mov ah, 0       ; Get keypress
     int 16h
     cmp ah, 4ch     ; Right arrow key
@@ -26,7 +26,7 @@ draw_snake:
     cmp ah, 4bh     ; Left arrow key
     je  move_left
 
-    ; Move the snake based on the current direction
+    ; Move the point based on the current direction
     cmp direction, 6
     je  move_right
     cmp direction, 8
@@ -39,26 +39,26 @@ draw_snake:
 move_right:
     add word ptr [x_pos], 1   ; Increment word at x_pos
     mov direction, 6
-    jmp draw_snake
+    jmp draw
 
 move_up:
     sub word ptr [y_pos], 1   ; Decrement word at y_pos
     mov direction, 8
-    jmp draw_snake
+    jmp draw
 
 move_down:
     add word ptr [y_pos], 1   ; Increment word at y_pos
     mov direction, 2
-    jmp draw_snake
+    jmp draw
 
 move_left:
     sub word ptr [x_pos], 1   ; Decrement word at x_pos
     mov direction, 4
-    jmp draw_snake
+    jmp draw
 
     int 20h           ; Exit program
 
 .data
-    x_pos dw 10    ; Initial X position of the snake
-    y_pos dw 10    ; Initial Y position of the snake
+    x_pos dw 10    ; Initial X position 
+    y_pos dw 10    ; Initial Y position
     direction db 6 ; Initial direction (6 = right)
